b7a403760ba5847ce1c9f2a677e28d26c181d348
Sankar Gorthi <sbgorthi@gmail.com>
2020-10-03 18:21:59 -0700

    exercise 2

--- src/__tests__/02.js
+++ src/__tests__/02.js
@@ -1,8 +1,8 @@
-import React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import App from '../final/02'
-// import App from '../exercise/02'
+import React from 'react'
+// import App from '../final/02'
+import App from '../exercise/02'
 
 afterEach(() => {
   window.localStorage.removeItem('name')
@@ -16,4 +16,10 @@ test('App works', () => {
   userEvent.type(screen.getByRole('textbox', {name: /name/i}), 'bob')
   screen.getByText(/hello.*bob/i)
   expect(window.localStorage.getItem('name')).toBe('bob')
+})
+
+test('App uses localstorage instead of prop', () => {
+  window.localStorage.setItem('name', 'bim')
+  render(<App initialName="radhika" />)
+  screen.getByText(/hello.*bim/i)
 })
--- src/exercise/02.js
+++ src/exercise/02.js
@@ -1,16 +1,16 @@
 // useEffect: persistent state
 // http://localhost:3000/isolated/exercise/02.js
 
-import React from 'react'
+import React, {useEffect} from 'react'
 
 function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
+  const [name, setName] = React.useState(
+    window.localStorage.getItem('name') || initialName,
+  )
 
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
+  useEffect(() => {
+    window.localStorage.setItem('name', name)
+  }, [name])
 
   function handleChange(event) {
     setName(event.target.value)
@@ -19,7 +19,7 @@ function Greeting({initialName = ''}) {
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
